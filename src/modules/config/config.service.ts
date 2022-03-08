ba9fcbe850a392e0bf200db346a229f7e25c0ecf
olamileke <olamileke.f@gmail.com>
2022-03-08 22:35:32 +0100

    feat: initial environment setup

---
+++ src/modules/config/config.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+import * as fs from 'fs';
+import * as path from 'path';
+
+@Injectable()
+export class ConfigService {
+  private envConfig: { [key: string]: string };
+
+  constructor() {
+    const envFile =
+      process.env.NODE_ENV === 'development'
+        ? '.env.development'
+        : '.env.production';
+    const envPath = path.resolve(__dirname, '../../../', envFile);
+    this.envConfig = dotenv.parse(fs.readFileSync(envPath));
+  }
+
+  get(key: string) {
+    return this.envConfig[key];
+  }
+}
