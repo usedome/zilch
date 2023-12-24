f7d7995c58a6c967d1cec06e8f855b1dc91ee28f
olamileke <olamileke.f@gmail.com>
2023-12-24 01:17:49 +0100

    feat: added ping api route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { UnguardedAuthRoute } from './utilities';
 
 @Controller()
@@ -9,5 +9,11 @@ export class AppController {
   @UnguardedAuthRoute()
   getHello(): string {
     return 'Hello from the BackMeUp Index File';
+  }
+
+  @Post('/ping')
+  @UnguardedAuthRoute()
+  ping() {
+    return { status: 'ok' };
   }
 }
